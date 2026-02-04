f4eb43d21a844b19549054a6273514fe0aad42ef
EvinKor <evin8917@gmail.com>
2026-02-04 14:27:17 +0800

    run build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/dist',
       server: {
         port: 3000,
         host: '0.0.0.0',
