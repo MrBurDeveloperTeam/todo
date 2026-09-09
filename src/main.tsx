6e6ffd26caf36a8c7f0662bd95c3ecbf36c40e46
Nizam Yuseri <nizamyuseri@MacBook-Air-Nizam.local>
2026-09-09 13:57:38 +0800

    fix tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import '@mrburdeveloperteam/molar-experience/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error("Could not find root element");
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
