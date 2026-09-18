c793ca30eaf77d5021f9c763341a1c29d5b05446
FONGQUANMING <quanmingfong0720@gmail.com>
2026-09-18 15:12:21 +0800

    change version

--- src/components/MolarAIFloat.jsx
+++ src/components/MolarAIFloat.jsx
@@ -1,5 +1,4 @@
 import { createTodoMolarAIFloat } from '@mrburdeveloperteam/pet-function/apps/todo';
 import { supabase } from '../lib/supabase';
-import { chatWithMolarAI, chatWithGroundedTodoFacts, routeTodoCapability } from '../services/geminiService';
-const MolarAIFloat = createTodoMolarAIFloat({ supabase, chatWithMolarAI, chatWithGroundedTodoFacts, routeTodoCapability });
+const MolarAIFloat = createTodoMolarAIFloat({ supabase });
 export default MolarAIFloat;
